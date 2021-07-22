ce7c11bf5bd55fa24101ff30a922dc6c6270e029
Max Stickels <maxwellstickels@gmail.com>
2021-07-21 19:33:04 -0700

    Attempt to allow access to exercise and stats pages on Heroku

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,14 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+    }); 
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+}); 
+
 router.get("/api/workouts", (req, res) => {
     Workout.find({})
         .sort({ date: -1 })
